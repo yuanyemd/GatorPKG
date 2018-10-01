177ba235a3632f5bdb57f741375c6f8da92c70b4
yuanyemd <yuanyemd@gmail.com>
2018-10-01 13:47:28 -0400

    first commit

---
+++ R/f.R
@@ -0,0 +1,12 @@
+##' Add up two numbers (Description)
+##'
+##' We want to add up two numbers, blalala... (Details)
+##' @title add two numbers
+##' @param x first number
+##' @param y second number
+##' @return sum of two numbers
+##' @author Caleb
+##' @export
+##' @examples
+##' f(1,2)
+f <- function(x, y) x + y
